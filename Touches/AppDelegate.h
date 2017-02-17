18fa0fbe39d031eac55607bf34790035b1cf4c42
admin <admin@Admins-MBP.kiwiup.in>
2017-02-17 17:27:26 +0530

    Initial Commit

---
+++ Touches/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Touches
+//
+//  Created by Admin on 17/02/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Touches/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Touches
+//
+//  Created by Admin on 17/02/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
